cc98db0895c468e03dd5067d626fc2a07bf29d2a
neil patel <neil@myindianweddingapp.com>
2014-04-23 10:50:32 +0100

    Form based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,41 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+	  	@article.save
+	  	redirect_to article_path(@article)
+	  	flash.notice = "Article '#{@article.title}' Created!"
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_url
+		flash.notice = "Article '#{@article.title}' Destroy!"
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to article_path(@article)
+	end
+
+
+	def article_params
+      params.require(:article).permit(:title, :body)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blogger2::Application.routes.draw do
 
     resources :articles
+
+    root to: 'articles#index'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
